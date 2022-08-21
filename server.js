297a164ddc133fe1ae34769d2f6c8e4d0dd47495
Victor Nunez <VN135766@gmail.com>
2022-08-21 18:25:45 -0400

    fixed issue

--- server.js
+++ server.js
@@ -5,16 +5,6 @@ const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-/*
-1. Go to Mongo Atlas
-2. Create the Mongo database for this activity
-3. Get the connection string for that database
-4. Go to project on Heroku
-5. Go to Settings
-6. Add a Config Var named MONGODB_URI
-7. The value of that var is the connection string from #3 above
-8. If you haven't needed to update the repo, just ReDeploy from Heroku
-*/
 
 const app = express();
 
@@ -28,7 +18,6 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
 });
 
 // routes
